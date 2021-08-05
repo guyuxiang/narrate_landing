d0780a3081764fa1183c16390c0ea2f3f0ce468a
Yuxiang Gu <lucas@ventureum.io>
2021-08-05 21:02:30 +0000

    foo

---
+++ src/config.js
@@ -0,0 +1,75 @@
+module.exports = {
+  email: 'yuxianggu@gmail.com',
+
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/guyuxiang',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/yuxianggu',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/yuxianggu',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/guyuxiang',
+    },
+    // {
+    //   name: 'Codepen',
+    //   url: 'https://codepen.io/bchiang7',
+    // },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Work',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+    {
+      name: 'Blog',
+      url: '/pensieve',
+    },
+    {
+      name: 'Knowledge base',
+      url: '/notes',
+    },
+  ],
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200, viewFactor = 0.25) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
